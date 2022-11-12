a4fa7f64640fbe1e2937e00a7a381417a647add1
pavinane <pavimega77@gmail.com>
2022-11-13 00:01:31 +0530

    create Content type to set Data from Json method

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
 
 const server = http.createServer((req,res) => {
-    res.write("Hello World");
-    res.end();
+    // res.write("Hello World");
+    // res.end();
+    res.writeHead(200,{"Content-Type":"application/json"});
+    res.end(
+        JSON.stringify({
+            data:"Hello World"
+        })
+    )
 })
 
 server.listen(3001);
